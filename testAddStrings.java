028171d3c8cc07c83354ac276891f39a883e7a5d
ANS1087 <103411532+ANS1087@users.noreply.github.com>
2022-11-22 14:53:42 +0530

    Add files via upload

---
+++ testAddStrings.java
@@ -0,0 +1,17 @@
+package jUnitTestPackage;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testAddStrings {
+
+	@Test
+	public void test()
+	{
+		JUnitFunctions junitstring = new JUnitFunctions();
+		String result= junitstring.addStrings("capstone", "project");
+		assertEquals ("capstoneproject", result);
+	}
+
+}
